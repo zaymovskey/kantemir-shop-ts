235f489faa688aa352165f596cd41dceeae86783
zaymovskey <zaymovskey@mail.com>
2024-01-30 14:33:32 +0300

    publicPath webpack

--- config/build/buildWebpackConfig.ts
+++ config/build/buildWebpackConfig.ts
@@ -14,7 +14,8 @@ export function buildWebpackConfig(options: IBuildOptions): webpack.Configuratio
     output: {
       filename: '[name].[contenthash].js',
       path: paths.build,
-      clean: true
+      clean: true,
+      publicPath: '/'
     },
     plugins: buildPlugins(options),
     module: {
