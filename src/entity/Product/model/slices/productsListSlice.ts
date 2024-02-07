3b7b48a806814df8016c88426b11333c54b22c47
zaymovskey <zaymovskey@mail.com>
2024-02-08 00:18:17 +0300

    TODO

--- src/entity/Product/model/slices/productsListSlice.ts
+++ src/entity/Product/model/slices/productsListSlice.ts
@@ -34,7 +34,7 @@ const productsListSlice = createSlice({
       .addCase(fetchProductsList.fulfilled, (state, action) => {
         state.isLoading = false;
         productsAdapter.addMany(state, action.payload);
-        // TODO: переделать это на бэкенд. Возвращать с бэка количество оставшихся товаров
+        // TODO: переделать это на бэкенд. Возвращать с бэка количество оставшихся товаров и выводить количество скелетонов по этому значению
         state.hasMore = action.payload.length === state.limit;
       })
       .addCase(fetchProductsList.rejected, (state, action) => {
